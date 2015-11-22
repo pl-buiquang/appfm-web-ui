53559d0f51634ac3eb94d4ddb1d9f4fe0ae6acd9
Paul Laptop <lynks05@hotmail.com>
2015-11-22 21:11:25 +0100

    ??

--- settings.php
+++ settings.php
@@ -4,13 +4,7 @@ define("DEBUG",true);
 define("LOG_HOME_PATH",'log/');
 define("PAGE_MAIN_TEMPLATE" ,"templates/html.tpl.php");
 define("TITLE","CPM");
-define("APP_HOME_ROUTE_FIX","/demo_big_data/www");
-define("BASE_DIR","/var/www/demo_big_data/www/");
-define("BASE_URL","http://localhost:80/demo_big_data/www/");
+define("APP_HOME_ROUTE_FIX","/cpm");
+define("BASE_DIR","/var/www/cpm/");
+define("BASE_URL","http://localhost:80/cpm/");
 define("CALLER_DEFAULT_MAIN_TEMPLATE",'front_default_main_template');
-
-
-define("DB_HOST","localhost");
-define("DB_USER" ,"lynks");
-define("DB_PWD","GbDyYmhy6SuDa6fj");
-define("DB_NAME" , "lynks");  
